a8fca24cd7f66f99feb952eb06cd362cb753f862
singhj01 <Jsingh01@manhattan.edu>
2019-04-11 21:00:13 -0400

    -y

--- controllerVNF/scripts/install.sh
+++ controllerVNF/scripts/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt install curl git
-sudo apt-get install mysql-server openssl
+sudo apt-get update -y
+sudo apt-get install -y curl git mysql-server openssl
 curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-sudo apt install nodejs
-apt-get install npm
+sudo apt install -y nodejs
+apt-get install -y npm
