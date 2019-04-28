f8db291f61576575490c4b1a2240a523c226376f
singhj01 <Jsingh01@manhattan.edu>
2019-04-28 15:57:53 -0400

    iptables

--- gatewayVNF/install.sh
+++ gatewayVNF/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update -y
-sudo apt-get install -y curl git openssl libssl-dev libjson0 libjson0-dev libpcap-dev texinfo libtool autoconf make
+sudo apt-get install -y curl git openssl libssl-dev libjson0 libjson0-dev libpcap-dev texinfo libtool autoconf make iptables
 git clone https://github.com/waverleylabs/fwknop.git
 
 cd fwknop
