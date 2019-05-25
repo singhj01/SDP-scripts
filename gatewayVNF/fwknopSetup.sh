c3c8c8110deaff11c698f4efc175b0ba209d60ba
leo104 <jsingh01@manhattan.edu>
2019-05-25 18:18:22 -0400

    dockerfiles updates

--- gatewayVNF/fwknopSetup.sh
+++ gatewayVNF/fwknopSetup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-echo $1
-yes Y | sudo scp -i controller@$1:/controller/1.key /gateway/
-yes Y | sudo scp -i controller@$1:/controller/1.crt /gateway/
-yes Y | sudo scp -i controller@$1:/controller/ca.crt /gateway/
-yes Y | sudo scp -i controller@$1:/controller/1.spa_keys /gateway/
+yes Y | sudo scp controller:/controller/1.key /gateway/
+yes Y | sudo scp controller:/controller/1.crt /gateway/
+yes Y | sudo scp controller:/controller/ca.crt /gateway/
+yes Y | sudo scp controller:/controller/1.spa_keys /gateway/
 
-
+sudo iptables -P INPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -S
 
 yes Y  | sudo cp access.conf /etc/fwknop/
 yes Y  | sudo cp fwknopd.conf /etc/fwknop/
@@ -14,7 +15,7 @@ yes Y  | sudo cp gate_sdp_ctrl_client.conf /etc/fwknop/
 yes Y  | sudo cp gate.fwknoprc /etc/fwknop/
 
 fwknopd -f 
-
+netcat -l 4444
 while true; do
     echo 'Dont close please'
 done
