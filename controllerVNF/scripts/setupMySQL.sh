2e1158f21cc3d1ed3f8c9c142e5e671bc7c3701f
nfv <jsingh01@manhattan.edu>
2019-07-16 22:21:01 -0400

    install script tweaks

--- controllerVNF/scripts/setupMySQL.sh
+++ controllerVNF/scripts/setupMySQL.sh
@@ -24,11 +24,11 @@ INSERT INTO service VALUES(1,'controller', 'controller' );
 INSERT INTO service VALUES(2,'tcp', 'tcp' );
 INSERT INTO service VALUES(3,'udp', 'udp' );
 
-INSERT INTO service_gateway (id,service_id, gateway_sdpid, protocol, port, nat_ip, nat_port) VALUES(1, 1, 1 ,'tcp',5000, '$(hostname -I)', 5000);
-INSERT INTO service_gateway (id,service_id, gateway_sdpid, protocol, port, nat_ip, nat_port) VALUES(2, 2, 1 ,'tcp',4444, 'gateway', 4443);
+INSERT INTO service_gateway (id,service_id, gateway_sdpid, protocol, port, nat_ip, nat_port) VALUES(1, 1, 1 ,'tcp',5000, 'controller', 5000);
+INSERT INTO service_gateway (id,service_id, gateway_sdpid, protocol, port, nat_ip, nat_port) VALUES(2, 2, 1 ,'tcp',4444, 'gateway', 4444);
 INSERT INTO sdpid_service VALUES (1, 1, 1);
 #this means that the gateway has access to service 1
 INSERT INTO sdpid_service VALUES (2, 1, 2);
-INSERT INTO sdpid_service VALUES(4, 3, 1);
-INSERT INTO sdpid_service VALUES(5, 3, 2);
+INSERT INTO sdpid_service VALUES(3, 3, 1);
+INSERT INTO sdpid_service VALUES(4, 3, 2);
 EOF
--- gatewayVNF/fwknopSetup.sh
+++ gatewayVNF/fwknopSetup.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-yes Y | sudo scp controller:/controller/1.key /gateway/
-yes Y | sudo scp controller:/controller/1.crt /gateway/
-yes Y | sudo scp controller:/controller/ca.crt /gateway/
-yes Y | sudo scp controller:/controller/1.spa_keys /gateway/
-
-sudo iptables -P INPUT DROP
+#sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD DROP
 sudo iptables -S
 
@@ -15,7 +10,7 @@ yes Y  | sudo cp gate_sdp_ctrl_client.conf /etc/fwknop/
 yes Y  | sudo cp gate.fwknoprc /etc/fwknop/
 
 fwknopd -f 
-netcat -l 4444
+#netcat -l 4444
 while true; do
     sleep 100
 done
